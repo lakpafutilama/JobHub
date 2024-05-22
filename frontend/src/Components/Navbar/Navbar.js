753f515a2dd601cf8d0611377b1c97f9cea66695
lakpafutilama <lakpa.lama4433@gmail.com>
2024-05-22 22:49:57 +0545

    text color for navbar li

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -2,38 +2,18 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { styled } from "@mui/system";
 import "./Navbar.css";
 
-const GlassAppBar = styled(AppBar)(({ theme }) => ({
-  background: 'rgba(255, 255, 255, 0.2)', // Semi-transparent background color
-  backdropFilter: 'blur(5px)', // Blur effect
-  WebkitBackdropFilter: 'blur(10px)', // For Safari support
-  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Box shadow for depth
-  position: 'fixed', // Fixed position to stick at the top
+const GlassAppBar = styled(AppBar)(({}) => ({
+  background: "rgba(255, 255, 255, 0.2)",
+  backdropFilter: "blur(5px)",
+  WebkitBackdropFilter: "blur(10px)",
+  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+  position: "fixed",
   zIndex: 1000,
 }));
-
-const NavList = styled('ul')({
-  display: 'flex',
-  listStyle: 'none',
-  margin: 0,
-  padding: 0,
-  '& li': {
-    marginRight: '20px',
-  },
-  '& a': {
-    textDecoration: 'none',
-    color: 'inherit',
-  },
-  '& button': {
-    background: 'none',
-    border: 'none',
-    color: 'inherit',
-    cursor: 'pointer',
-  },
-});
 
 function Navbar({ toggleSignIn }) {
   const location = useLocation();
@@ -42,10 +22,10 @@ function Navbar({ toggleSignIn }) {
       <Toolbar>
         <div className="logo" style={{ flexGrow: 1 }}>
           <a href="#home">
-            <img src="/assets/logo.png" alt="JOBHUB" style={{ height: '40px' }} />
+            <img src="/assets/logo.png" alt="JOBHUB" />
           </a>
         </div>
-        <NavList>
+        <ul className="navbar">
           <li>
             <a href="/">Home</a>
           </li>
@@ -58,24 +38,18 @@ function Navbar({ toggleSignIn }) {
           <li>
             <a href="#contact">Contact</a>
           </li>
-          {location.pathname === '/' ? (
+          {location.pathname === "/" ? (
             <li>
-              <a href="#" onClick={toggleSignIn}>Signin</a>
+              <a href="#" onClick={toggleSignIn}>
+                Signin
+              </a>
             </li>
-
           ) : (
-
             <li>
-              <Button >
-                Menu
-              </Button>
+              <Button>Menu</Button>
             </li>
           )}
-
-          {/* <li>
-            <Button color="inherit" onClick={toggleSignIn}>Signin</Button>
-          </li> */}
-        </NavList>
+        </ul>
       </Toolbar>
     </GlassAppBar>
   );
